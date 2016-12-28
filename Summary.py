2474d250106731d006a8fe3c41f980fffc0a1ffc
siddsax <ss.siddharthasaxena@gmail.com>
2016-12-29 04:31:54 +0530

    lec5 completed

--- Summary.py
+++ Summary.py
@@ -2,34 +2,34 @@ class Lec_3:
    def __init__(self): 
    def Queues(arrays):
    def Queues(lists):
-   	    string s = "Need_head_and_tail_pointer."
-   	    string q = "Head of list is front of queue and tail \
+   	    string_s = "Need_head_and_tail_pointer."
+   	    string_q = "Head of list is front of queue and tail \
    	               of list is back of queue."
-        string r = "To remove the last element of the list \
+        string_r = "To remove the last element of the list \
                    we need the pointer to second last element"
    def Double_ended_Queues(doubly_linkedlists):
-        string j = "can add/remove fromboth ends"
+        string_j = "can add/remove fromboth ends"
    def Stack(Double_ended_queues):   	
    def Queues(Circular_lists):
-   	    string i = "needs only one head pointer"
-   	    string m = "to add a new element at head, \
+   	    string_i = "needs only one head pointer"
+   	    string_m = "to add a new element at head, \
    	               add it just after head and copy"
-   	    string k = "to remove an element, remove \
+   	    string_k = "to remove an element, remove \
    	               head and then copy \
    	               correspondingly and change head \
    	               pointer"       
    def vector(arrays):
-        string l = "add a new element at some rank \
+        string_l = "add a new element at some rank \
                     and shift others"   	
    def vector(Doubly_linked_lists):
    	    string u = "Find where to insert O(N) and insert O(1)"
    def list(list)
-   	    string y = "it is the data type that is implemented \
+   	    string_y = "it is the data type that is implemented \
    	                using linked or doubly linked list data \
    	                structures"
    self.position = "a pointer to something"	                
    def Sequence():
-   	    string p = "has both ranks and positions, can be\
+   	    string_p = "has both ranks and positions, can be\
    	                implemeneted via array of pointer and \
    	                a data structure which holds the rank \
    	                in itself as well."
@@ -47,7 +47,8 @@ class Lec_4:
                             list. A f unction maps each \
                             element to a number via which it is \
                             added to an array " 
-        self.load_factor = "ratio of number of elements in the array\
+        self.load_fdef Open_addressing:
+        	actor = "ratio of number of elements in the array\
                             and slots per element" 
         self.sorted_loading = "We keep the linked list sorted"                                           
         self.time_insertion = O(1+alpha/2-1/2*m)
@@ -56,11 +57,54 @@ class Lec_4:
     	def size():
     	def findElement(key):
     	    string j = "search using a key"
-class Lec_4:
-	self.Hash_functions = "Maps to a hash table indice"
-	self.Hash_code_map = "Key -> integer"
-	self.compression_map = "integer -> indice"
-	self.Polynomial_maps = "to convert string to integers then make a poynmial \
-	                        that has the coefficient as the ascii value of \
-	                         string and put some value of x, 33,34... are good"
- 
+class Lec_5:
+	  self.Hash_functions = "Maps to a hash table indice"
+	  class Hash_code_map:
+	      self.defination = "Key -> integer"
+	      def Polynomial_accumulation(key):
+            j = "to convert string to integers make a polynmial \
+	              that has the coefficient as the ascii value of \
+	              string and put some value of x. 33,34... are good"
+    class Compression_map:
+        self.defination = "integer -> indice"
+        def remainder:
+            divisor_1 = "don't take power of two as it is like \
+                      dealing with last few digits of the number"
+            divisor_2 = "take it as a prime to prevent patterns"
+        def map_bw_0_and_1:
+            step_1 = "map from (0 to K) to (0 to KA), A < 1"
+            step_2 = "take fractional part"
+            step_3 = "map to 0 to m-1 by multiplying with m"
+        def MAD:
+            define = "|ak+b|modN, and N are coprime"              
+        def Universal_Hashing:
+        class Open_Addresing:
+        	  self.defination = "Add probe number 'i' to the hash \
+        	                    function. We do not do chaining, rather there is just a single list or array."
+        	  def Linear_probe():
+        	  	  s = "give a key by the hash function and move ahead if that is already filled."
+        	      search = "move until you find an empty space after getting the hash position. "              
+        	      deletion = "put a cross rather than emptying it. We can put data here if required actually."
+        	  def doubleHashing():
+        	      s = "two hashing function, the second one chooses the offset required for probing" 
+        	  def analysisDoubleHashing():
+        	      load_factor = alpha 
+        	      empty = 1-load_factor
+        	      Exp_probes_to_find_empty_loation = 1/empty       
+                time_for_one_probe = -(1/alpha)*log(1-alpha)
+                return "Cant Understand Properly"
+class Lec_5:
+	  self.root = "top"
+	  self.node = "nodes which has a child"
+	  self.leaf = "nodes with no children"
+	  self.depth_or_level = "the position of a node vertically"
+	  self.height = "Max depth of a node"
+	  self.ordered_tree = "children are ordered, ascending or descending"
+	  self.binary_tree = "one or two children only"
+	  self.Complete_binary_tree = "ith level has 2^i nodes"
+	  self.height_binary_tree = "between n and log(n+1)-1"
+	  self.no_of_leaves = "it is always less than 1 + no of internal nodes proved by induction on this inequality"
+
+            
+
+
